64f9923a8713f886f9501e6efbff0ff5179cca2d
EC2 Default User <ec2-user@ip-172-31-24-199.us-west-2.compute.internal>
2022-10-21 09:32:35 +0000

    バリデーションを追加

--- Gemfile
+++ Gemfile
@@ -51,3 +51,6 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'bootsnap', '>= 1.4.2', require: false
+gem 'rails-i18n'
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,2 +1,18 @@
 class Schedule < ApplicationRecord
+     validates :title, presence: true
+     validates :start,  presence: true
+     validates :end, presence: true
+     validate :start_end_check
+     validate :startday
+     
+     def start_end_check
+        return if self.start.blank?
+        return if self.end.blank?
+        errors.add(:end, "は開始日より前は登録できません。") unless self.start < self.end 
+     end
+     
+     def startday
+       return if self.start.blank?
+       errors.add(:start, "は今日以降の日付を選択してください") if start < Date.today
+     end
 end
